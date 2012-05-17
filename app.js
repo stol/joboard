a0c5e38e747709d8187720074331e085db0585c5
Lord Stol <alexis.faye@hotmail.fr>
2012-05-17 12:37:04 +0200

    on renvoie maintenant du json propre + bug fix dates + affichages de ttes les infos

--- app.js
+++ app.js
@@ -6,182 +6,125 @@ var http    = require('http'),
     moment  = require('moment'),
     _       = require('underscore'),
     jenkins = require('jenkins'),
-    iconv = require('iconv-lite')
+    iconv   = require('iconv-lite')
 
-var providers = [
-    {
-        name: 'Alsacreation',
-        url_list : 'http://emploi.alsacreations.com/offres.html',
-        url_item : 'http://emploi.alsacreations.com/',
-        date_formater: 'DD MMM YYYY',
-        date_lang : 'fr',
-        selectors: {
-            row    : 'table.results tr',
-            label  : '.intitule',
-            date   : 'td:eq(2)',
-            company: 'td:eq(0) strong',
-            href   : '.intitule',
-            place  : 'td:eq(1)'
-        },
-        place_contents: 5
-    }
-    ,{
-        name: 'Lolix',
-        url_list : 'http://fr.lolix.org/search/offre/search.php?page=0&mode=find&posteid=0&regionid=0&contratid=0',
-        url_item : 'http://fr.lolix.org/search/offre/',
-        enc: "ISO-8859-1",
-        date_formater: 'DD MMMM YYYY',
-        date_lang : 'en',
-        selectors: {
-            row    : 'td.Contenu table:eq(1) tr:gt(0)',
-            label  : 'td:eq(2)',
-            date   : 'td:eq(0)',
-            company: 'td:eq(1)',
-            href   : 'td:eq(2) a',
-            place  : 'td:eq(3)'
-        }
-    },
-    {
-        name: 'Remixjobs',
-        url_list : 'http://remixjobs.com',
-        date_formater: "DD MMM YYYY",
-        date_lang : 'fr',
-        selectors: {
-            row    : '.job-infos',
-            label  : '.job-title',
-            date   : '.job-details-right',
-            company: '.company',
-            href   : '.job-link',
-            place  : '.workplace'
-        }
-    },
-    /*
-    {
-        name: 'Humancoders Ruby',
-        url_list : 'http://jobs.humancoders.com/ruby',
-        url_item : 'http://jobs.humancoders.com/',
-        date_formater: "DD MMM YYYY",
-        date_year : 2012,
-        date_lang : 'fr',
-        selectors: {
-            row    : '.job',
-            label  : '.job_title',
-            date   : '.date',
-            company: '.second .left span',
-            href   : '.job_title a',
-            place  : '.location'
-        }
-    },
-    {
-        name: 'Humancoders Javascript',
-        url_list : 'http://jobs.humancoders.com/javascript',
-        url_item : 'http://jobs.humancoders.com/',
-        date_formater: "DD MMM YYYY",
-        date_year : 2012,
-        date_lang : 'fr',
-        selectors: {
-            row    : '.job',
-            label  : '.job_title',
-            date   : '.date',
-            company: '.second .left span',
-            href   : '.job_title a',
-            place  : '.location'
-        }
-    },
-    */
-    {
-        name: 'French web',
-        url_list : 'http://emploi.frenchweb.fr/',
-        date_formater: "DD MMM YYYY",
-        date_lang : 'fr',
-        selectors: {
-            row    : '.job',
-            label  : '.title strong',
-            date   : '.date',
-            company: '.title > a',
-            href   : '.title strong a',
-            place  : '.location'
-        }
-    },
-    {
-        name: 'Express-board',
-        url_list : 'http://www.express-board.fr',
-        date_formater: "DD MMM YYYY",
-        date_lang : 'fr',
-        selectors: {
-            row    : '.stb:eq(0) tbody tr',
-            label  : 'td:eq(0) a',
-            date   : 'td:eq(3)',
-            company: 'td:eq(1) a',
-            href   : 'td:eq(0) a',
-            place  : 'td:eq(2) a'
-        }
-    }
-    
-]
+var providers = []
 
-var offres = {},
+providers.push({
+    name: 'Alsacreation',
+    url_list : 'http://emploi.alsacreations.com/offres.html',
+    url_item : 'http://emploi.alsacreations.com/',
+    date_formater: 'DD MMM YYYY',
+    date_lang : 'fr',
+    selectors: {
+        row    : 'table.results tr',
+        label  : '.intitule',
+        date   : 'td:eq(2)',
+        company: 'td:eq(0) b',
+        href   : '.intitule',
+        place  : 'td:eq(1)'
+    },
+    place_contents: 2
+})
+
+providers.push({
+    name: 'Lolix',
+    url_list : 'http://fr.lolix.org/search/offre/search.php?page=0&mode=find&posteid=0&regionid=0&contratid=0',
+    url_item : 'http://fr.lolix.org/search/offre/',
+    enc: "ISO-8859-1",
+    date_formater: 'DD MMMM YYYY',
+    date_lang : 'en',
+    selectors: {
+        row    : 'td.Contenu table:eq(1) tr:gt(0)',
+        label  : 'td:eq(2)',
+        date   : 'td:eq(0)',
+        company: 'td:eq(1)',
+        href   : 'td:eq(2) a',
+        place  : 'td:eq(3)'
+    }
+})
+
+providers.push({
+    name: 'Remixjobs',
+    url_list : 'http://remixjobs.com',
+    date_formater: "DD MMM YYYY",
+    date_lang : 'fr',
+    selectors: {
+        row    : '.job-infos',
+        label  : '.job-title',
+        date   : '.job-details-right',
+        company: '.company',
+        href   : '.job-link',
+        place  : '.workplace'
+    }
+})
+
+providers.push({
+    name: 'French web',
+    url_list : 'http://emploi.frenchweb.fr/',
+    url_item : '',
+    date_formater: "DD MMM YYYY",
+    date_lang : 'fr',
+    selectors: {
+        row    : '.job',
+        label  : '.title strong',
+        date   : '.date',
+        company: '.title > a',
+        href   : '.title strong a',
+        place  : '.location'
+    }
+})
+
+providers.push({
+    name: 'Express-board',
+    url_list : 'http://www.express-board.fr',
+    date_formater: "DD MMM YYYY",
+    date_lang : 'fr',
+    selectors: {
+        row    : '.stb:eq(0) tbody tr',
+        label  : 'td:eq(0) a',
+        date   : 'td:eq(3)',
+        company: 'td:eq(1) a',
+        href   : 'td:eq(0) a',
+        place  : 'td:eq(2) a'
+    }
+})
+
+providers.push({
+    name: 'Humancoders Ruby',
+    url_list : 'http://jobs.humancoders.com/',
+    date_formater: ["DD MMM YYYY", "DD MMMM YYYY"],
+    date_year : 2012,
+    date_lang : 'fr',
+    selectors: {
+        row    : '.job',
+        label  : '.job_title',
+        date   : '.date',
+        company: '.second .left span',
+        href   : '.job_title a',
+        place  : '.location'
+    }
+})
+
+var offres = [],
     offres_nb = 0,
     providers_done = 0
-
-function load_provider(p, res)
-{
-    moment.lang(p.date_lang);
-    var max = 100; 
-    var i = 0;
-
-    request({url: p.url_list}, function (error, response, body) {
-        if (p.enc)
-            body = iconv.encode(body, "UTF-8");
-        jsdom.env({html: body, scripts: ['http://code.jquery.com/jquery-1.7.2.min.js']}, function(err, window){
-            var $ = window.jQuery;
-            
-            $(p.selectors.row).each(function(i,row){
-                var offre = {
-                    label : $(row).find(p.selectors.label).text().trim(),
-                    company: $(row).find(p.selectors.company).text().trim(),
-                    href: (p.url_item ? p.url_item : p.url_list)+$(row).find(p.selectors.href).attr("href"),
-                    lieu: p.place_contents
-                        ? $(row).find(p.selectors.place).contents().eq(p.place_contents).text()
-                        : $(row).find(p.selectors.place).text().trim(),
-                    source: p.name,
-                    fresh: 1
-                }
-
-                var date_str = $(row).find(p.selectors.date).text().trim() + (p.date_year ? p.date_year : '');
-
-                if (date_str.indexOf('heures')>=0){
-                    offre.ts = moment().subtract('hours', parseInt(date_str.substring(7),10)).valueOf();
-                }
-                else if (date_str.indexOf('jour')>=0){
-                    offre.ts = moment().subtract('days', parseInt(date_str,10)).valueOf();
-                }
-                else {
-                    offre.ts = moment(date_str,p.date_formater).valueOf()
-                }
-
-                var id = jenkins.hash(offre.href);
-
-                if (offres[id])
-                    offre.fresh = 0;
-
-                res.write(",\n"+JSON.stringify(offre));
-
-                offres[id] = offre;
-
-                offres_nb++;
-
-                if (++i > max)
-                    return false;
-            });
-            console.log("done "+(providers_done+1)+"/"+providers.length)
-            if (++providers_done == providers.length){
-                res.end(']');
-            }
-        });
-    });
-}
-
+var UTF8 = {
+    encode: function(s){
+        for(var c, i = -1, l = (s = s.split("")).length, o = String.fromCharCode; ++i < l;
+            s[i] = (c = s[i].charCodeAt(0)) >= 127 ? o(0xc0 | (c >>> 6)) + o(0x80 | (c & 0x3f)) : s[i]
+        );
+        return s.join("");
+    },
+    decode: function(s){
+        for(var a, b, i = -1, l = (s = s.split("")).length, o = String.fromCharCode, c = "charCodeAt"; ++i < l;
+            ((a = s[i][c](0)) & 0x80) &&
+            (s[i] = (a & 0xfc) == 0xc0 && ((b = s[i + 1][c](0)) & 0xc0) == 0x80 ?
+            o(((a & 0x03) << 6) + (b & 0x3f)) : o(128), s[++i] = "")
+        );
+        return s.join("");
+    }
+};
 
 var app = express.createServer();
 app.set("view options", {layout: false});
@@ -193,15 +136,95 @@ app.register('.html', {
     }
 });
 app.use(express.static(__dirname + '/public'));
+app.get("/test", function(req, res){
+    moment.lang("fr");
+    var a = moment("16 mai 2012", "DD MMM YYYY").format("DD/MM/YY");
+    res.end(a)
 
+})
 app.get('/offres', function(req, res){
     providers_done = 0;
+    offres = [];
+    offres_nb = 0;
+
     res.charset = 'UTF-8';
     res.header('Content-Type', 'application/json');
-    res.write('[null')
     //load_provider(providers[0], res);
     _.each(providers, function(p){
-        load_provider(p, res);
+        var max = 100; 
+        var i = 0;
+
+        request({url: p.url_list}, function (error, response, body) {
+            jsdom.env({html: body, scripts: ['http://code.jquery.com/jquery-1.7.2.min.js']}, function(err, window){
+                var $ = window.jQuery;
+                moment.lang(p.date_lang);
+                
+                $(p.selectors.row).each(function(i,row){
+                    var offre = {
+                        label : $(row).find(p.selectors.label).text().trim(),
+                        company: $(row).find(p.selectors.company).text().trim(),
+                        href: (p.url_item !== undefined ? p.url_item : p.url_list)+$(row).find(p.selectors.href).attr("href"),
+                        lieu: p.place_contents
+                            ? $(row).find(p.selectors.place).contents().eq(p.place_contents).text()
+                            : $(row).find(p.selectors.place).text().trim(),
+                        source: p.name,
+                        fresh: 1
+                    }
+                    /*
+                    if (p.enc){
+                        console.log("avant : "+offre.label)
+                        offre.label = utf8_decode(offre.label);
+                        console.log("apres : "+offre.label)
+                    }
+                    */
+
+                    var date_str = $(row).find(p.selectors.date).text().trim() + (p.date_year ? ' '+p.date_year : '');
+                    date_str = date_str.toLowerCase();
+                    var res = null;
+                    // Si la date est de la forme "x minute(s)"
+                    if (res = date_str.match(/(\d+) minutes?/i)){
+                        offre.ts = moment().subtract('minutes', res[1]).valueOf();
+                    }
+                    // Si la date est de la forme "x heure(s)"
+                    else if (res = date_str.match(/(\d+) heures?/i)){
+                        offre.ts = moment().subtract('hours', res[1]).valueOf();
+                    }
+                    // Si la date est de la forme "x jour(s)"
+                    else if (res = date_str.match(/(\d+) jours?/i)){
+                        offre.ts = moment().subtract('days', res[1]).valueOf();
+                    }
+                    // Si la date est de la forme "x mois"
+                    else if (res = date_str.match(/(\d+) mois/i)){
+                        offre.ts = moment().subtract('months', res[1]).valueOf();
+                    }
+                    // sinon, on suit le formater du provider
+                    else{
+                        // spécial pour human coders, on met "mar" en "mars", car leur "short month" n'est pas usuel
+                        date_str = date_str.replace(" mar ", " mars ");
+                        offre.ts = moment(date_str,p.date_formater).valueOf()
+                    }
+
+                    offre.id = jenkins.hash(offre.href);
+
+                    offres.push(offre);
+
+                    offres_nb++;
+
+                    if (++i > max)
+                        return false;
+                });
+                if (++providers_done == providers.length){
+
+                    offres = _.sortBy(offres, function(o){
+                        return -o.ts;
+                    })
+
+                    res.json(offres);
+                }
+            });
+        });
+
+
     });
 
     //res.send('Hello World');
@@ -214,21 +237,3 @@ app.get('/', function(req, res){
 
 
 app.listen(3000);
-
-/*
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-  	
-
-	request({url: "http://remixjobs.com/"}, function (error, response, body) {
-		jsdom.env({html: body, scripts: ['http://code.jquery.com/jquery-1.7.2.min.js']}, function(err, window){
-            var $ = window.jQuery; 
-            console.log($('title').text());
-            res.end($('title').text());
-        });
-	});
-
-}).listen(8124);
-
-console.log('Server running at http://127.0.0.1:8124/');
-*/
